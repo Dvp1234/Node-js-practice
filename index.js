636eea9d2d3d279e3f6491c165b1ee8789fa1c56
Dvp1234 <88303776+Dvp1234@users.noreply.github.com>
2022-09-19 14:09:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//  the hello world program
+// console.log('Hello World');
+
+const calculator = require('./calc');
+
+let r  = calculator.sum(2,3)
+console.log(r)
+let s  = calculator.sub(20,3)
+console.log(s)
+let t  = calculator.multi(2,3)
+console.log(t)
+let u  = calculator.divide(7,3)
+console.log(u)
+
